c2c39f6e004612fd6e7421e42a27def024fbd104
ALISHAN <alishan92@gmail.com>
2014-12-07 19:07:43 -0500

    Add functionality to select any bus line

--- app/controllers/busses_controller.rb
+++ app/controllers/busses_controller.rb
@@ -1,13 +1,16 @@
 class BussesController < ApplicationController
 
 	def index
+		f = File.open("app/assets/javascripts/route_names.json", "r")
+		@bus_routes = JSON.parse(f.read)
+		f.close
 	end
 
 	def show
 		f = File.open("app/assets/javascripts/#{params[:id]}.json", "r")
-		@route = f.read
+		@schedule = f.read
 		f.close
-		render :json => @route 
+		render :json => @schedule 
 	end
 
 end
