eaab7be6eadd05370e434aad0cfd4ea3c636adf4
ALISHAN <alishan92@gmail.com>
2014-12-07 22:44:58 -0500

    Changed json path to reflect new location

--- app/controllers/busses_controller.rb
+++ app/controllers/busses_controller.rb
@@ -1,13 +1,13 @@
 class BussesController < ApplicationController
 
 	def index
-		f = File.open("app/assets/javascripts/route_names.json", "r")
+		f = File.open("app/assets/javascripts/schedules/route_names.json", "r")
 		@bus_routes = JSON.parse(f.read)
 		f.close
 	end
 
 	def show
-		f = File.open("app/assets/javascripts/#{params[:id]}.json", "r")
+		f = File.open("app/assets/javascripts/schedules/#{params[:id]}.json", "r")
 		@schedule = f.read
 		f.close
 		render :json => @schedule 
