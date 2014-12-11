7bac771734345fe9da987dd042bc78115a1c92d2
ALISHAN <alishan92@gmail.com>
2014-12-11 15:59:57 -0500

    Create model for bus routes

---
+++ app/models/bus.rb
@@ -0,0 +1,10 @@
+class Bus
+	include Mongoid::Document
+	include Mongoid::Attributes::Dynamic
+
+	# Schema
+	# name: "110A Central Pickering"
+	# route: "110A"
+	# day: "weekday" or saturday or sunday
+	# stops: [{name: , lat: , lon: , times: []},{},{}]
+end
