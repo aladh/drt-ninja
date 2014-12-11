2b3fdde8c0ea38022ce51ddfd77b82096216039f
ALISHAN <alishan92@gmail.com>
2014-12-11 17:06:08 -0500

    Get locations from JS data instead of HTML

--- app/controllers/busses_controller.rb
+++ app/controllers/busses_controller.rb
@@ -4,14 +4,6 @@ class BussesController < ApplicationController
 		f = File.open("app/assets/javascripts/route_names.json", "r")
 		@route_names = JSON.parse(f.read)
 		f.close
-		@test_coords = []
-		@test_coords[0] = 43.840035
-		@test_coords[1] = -79.123702 
-		stops = Bus.where(route: "110").find_by(day: "weekday").stops
-  	@locations = []
-  	stops.each do |stop|
-  		@locations.push({latitude: stop["lat"], longitude: stop["lon"]})
-		end
 	end
 
 	def show
