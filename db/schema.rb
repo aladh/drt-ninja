ca9023993fe41b40697aa99d0ca917a5f37c8d8e
ALISHAN <alishan92@gmail.com>
2014-12-14 14:57:10 -0500

    Change latitude and longitude data type
    From decimal to float

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141212210516) do
+ActiveRecord::Schema.define(version: 20141212224103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(version: 20141212210516) do
   create_table "stops", force: true do |t|
     t.string   "name"
     t.string   "location"
-    t.decimal  "latitude"
-    t.decimal  "longitude"
+    t.float    "latitude"
+    t.float    "longitude"
     t.string   "times",      default: [], array: true
     t.datetime "created_at"
     t.datetime "updated_at"
