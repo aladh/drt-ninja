6e607f0093872341b39e8f109c267552cfb8ca09
ALISHAN <alishan92@gmail.com>
2014-12-18 21:18:43 -0500

    Find next stop time

--- app/controllers/busses_controller.rb
+++ app/controllers/busses_controller.rb
@@ -4,20 +4,26 @@ class BussesController < ApplicationController
 	def index
 		if request.xhr? && params[:lat] && params[:lon]
 			@stops = Stop.near([params[:lat], params[:lon]], 0.2, units: :km).to_a
-			p @stops.count
 			@stops.delete_if do |stop|
 				stop.bus.day == 'saturday' || stop.bus.day == 'sunday'
 			end
+			p @stops.count
 			@stops = @stops.group_by {|stop| stop.bus.route}
 			@stops.keys.each {|key| @stops[key] = @stops[key][0]}
 			@nearby = []
+			# Convert current time to 2014-01-01
+			current_time = Time.parse(Time.now.to_s[11..-7], Time.parse("2014-01-01")).getutc
+			p current_time
+			p @stops
 			@stops.keys.each do |key| 
-				# Convert current time to 2014-01-01
-				current_time = Time.parse(Time.now.to_s[11..-7], Time.parse("2014-01-01")).getutc
-				next_time = nil
+				next_time = Time.parse(Time.now.to_s[11..-7], Time.parse("2014-01-01")).getutc.to_f * 1000
 				@stops[key].coded_times.each do |time|
-						next_time = time if current_time < time
+					p time
+					if current_time < time
+						next_time = time
+						p true
 						break
+					end
 				end
 				@nearby.push({route: key, stop: @stops[key], time: next_time})
 			end
