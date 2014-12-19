f1faca63cbba847f0b5911d50e5637af9d794021
ALISHAN <alishan92@gmail.com>
2014-12-18 22:51:42 -0500

    Don't send time if nil

--- app/controllers/busses_controller.rb
+++ app/controllers/busses_controller.rb
@@ -7,25 +7,22 @@ class BussesController < ApplicationController
 			@stops.delete_if do |stop|
 				stop.bus.day == 'saturday' || stop.bus.day == 'sunday'
 			end
-			p @stops.count
 			@stops = @stops.group_by {|stop| stop.bus.route}
 			@stops.keys.each {|key| @stops[key] = @stops[key][0]}
 			@nearby = []
 			# Convert current time to 2014-01-01
 			current_time = Time.parse(Time.now.to_s[11..-7], Time.parse("2014-01-01")).getutc
-			p current_time
-			p @stops
 			@stops.keys.each do |key| 
-				next_time = Time.parse(Time.now.to_s[11..-7], Time.parse("2014-01-01")).getutc.to_f * 1000
+				next_time = nil
 				@stops[key].coded_times.each do |time|
-					p time
 					if current_time < time
-						next_time = time
-						p true
+						next_time = Time.parse(time.getlocal.to_s[11..-7])
 						break
 					end
 				end
-				@nearby.push({route: key, stop: @stops[key], time: next_time})
+				if next_time != nil
+					@nearby.push({route: key, stop: @stops[key], time: next_time}) 
+				end
 			end
 			render :json => @nearby
 		else
